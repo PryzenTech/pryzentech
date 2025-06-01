f2cc1d8db16f268b7c1c2fc6bd5c88a753642941
Prnce Tyagi <princetyagi1901@gmail.com>
2025-06-01 10:22:40 +0530

    initial changes

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+
+const App = () => {
+  return (
+    <div className='text-4xl'>
+      <Navbar />
+      <Routes>
+        <Route />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
