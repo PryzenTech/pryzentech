e38355757ac4196bf7fe6442dd9404cdaa8f300b
“Kunal <kunalkoushik44@gmail.com>
2025-06-01 11:27:21 +0530

    ug

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
+import {BrowserRouter,Routes,Route, useNavigate ,} from 'react-router-dom'
 import Navbar from './Components/Navbar'
+import HomePage from './Pages/HomePage'
+import {Navigate} from 'react-router-dom'
 
 const App = () => {
+
   return (
+    <BrowserRouter>
     <div className='text-4xl'>
       <Navbar />
       <Routes>
-        <Route />
+        <Route path="/" element={<HomePage/>} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </div>
+    </BrowserRouter>
   )
 }
 
