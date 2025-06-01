3cb9b93f2e682ec0b962fe8a6cc1a2ba13ff9319
PryzenTech <info.pryzentech@gmail.com>
2025-06-01 11:17:57 +0530

    navbar by prince tyagi

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
-import React from 'react'
-import Navbar from './Components/Navbar'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './Components/Navbar';
+
+const Home = () => <div className="p-4 text-xl">Welcome to Home</div>;
+const About = () => <div className="p-4 text-xl">About Us</div>;
+const MobileAppDev = () => <div className="p-4 text-xl">Mobile App Development</div>;
+// Add more dummy pages as needed...
 
 const App = () => {
   return (
-    <div className='text-4xl'>
-      <Navbar />
-      <Routes>
-        <Route />
-      </Routes>
-    </div>
-  )
-}
+    <Router>
+      <div className="text-4xl">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/mobile-app-development-company" element={<MobileAppDev />} />
+          {/* Add more Route components for other paths you defined in Navbar */}
+        </Routes>
+      </div>
+    </Router>
+  );
+};
 
-export default App
+export default App;
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div className='m-20'>HomePage</div>
   )
 }
 
